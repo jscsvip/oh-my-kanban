d4a4626af6d64eb9f638bd90aa6ae150f45e9d32
李龙 <lilong2576@hanweb.com>
2024-11-30 13:31:30 +0800

    style: 卡片样式布局

--- src/App.js
+++ src/App.js
@@ -11,9 +11,21 @@ function App() {
       <main className='kanban-board'>
           <section className='kanban-column column-todo'>
             <h2>待处理</h2>
+            <ul>
+              <li className='kanban-card'>
+                <div className='card-title'>开发任务</div>
+                <div className='card-status'>22-05-22 18:15</div>
+              </li>
+            </ul>
           </section>
           <section className='kanban-column column-ongoing'>
             <h2>进行中</h2>
+            <ul>
+              <li className='kanban-card'>
+                <div className='card-title'>开发任务</div>
+                <div className='card-status'>22-05-22 18:15</div>
+              </li>
+            </ul>
           </section>
           <section className='kanban-column column-done'>
             <h2>已完成</h2>
