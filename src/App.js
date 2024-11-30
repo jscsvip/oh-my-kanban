4d2bb43e5fd55402463aea5b072eba9932aa11c3
李龙 <lilong2576@hanweb.com>
2024-11-30 13:39:39 +0800

    feat: 封装看板组件,遍历组件

--- src/App.js
+++ src/App.js
@@ -1,6 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
+const todoList = [
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  },
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  },
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  },
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  }
+]
+const ongoingList = [
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  },
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  }
+]
+const doneList = [
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  },
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  },
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  }
+]
+
+const KanbanCard = ({title,status}) => {
+  return (
+    <li className='kanban-card'>
+      <div className='card-title'>{title}</div>
+      <div className='card-status'>{status}</div>
+    </li>
+  )
+}
 function App() {
   return (
     <div className="App">
@@ -12,23 +63,37 @@ function App() {
           <section className='kanban-column column-todo'>
             <h2>待处理</h2>
             <ul>
-              <li className='kanban-card'>
+            {/* {
+              new Array(5).fill(0).map((item, index) => {
+                return <li className='kanban-card'>
                 <div className='card-title'>开发任务</div>
                 <div className='card-status'>22-05-22 18:15</div>
               </li>
+              })
+            } */}
+             {/* {todoList.map((item, index) => {
+              return <KanbanCard title={item.title} status={item.status} />
+             })} */}
+             {todoList.map((item, index) => {
+              return <KanbanCard {...item} />
+             })}
             </ul>
           </section>
           <section className='kanban-column column-ongoing'>
             <h2>进行中</h2>
             <ul>
-              <li className='kanban-card'>
-                <div className='card-title'>开发任务</div>
-                <div className='card-status'>22-05-22 18:15</div>
-              </li>
+            {ongoingList.map((item, index) => {
+              return <KanbanCard {...item} />
+             })}
             </ul>
           </section>
           <section className='kanban-column column-done'>
             <h2>已完成</h2>
+            <ul>
+            {doneList.map((item, index) => {
+              return <KanbanCard {...item} />
+             })}
+            </ul>
           </section>
         </main>
     </div>
