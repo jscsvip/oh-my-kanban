82e6538e5f5753f5abce44fc67be435c4c18ab07
李龙 <lilong2576@hanweb.com>
2024-11-30 13:46:09 +0800

    feat: 添加状态样式

--- src/App.js
+++ src/App.js
@@ -17,6 +17,18 @@ const todoList = [
   {
     title: '开发任务',
     status: '22-05-22 18:15'
+  },
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  },
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
+  },
+  {
+    title: '开发任务',
+    status: '22-05-22 18:15'
   }
 ]
 const ongoingList = [
@@ -43,12 +55,23 @@ const doneList = [
     status: '22-05-22 18:15'
   }
 ]
-
+// 看板组件
 const KanbanCard = ({title,status}) => {
   return (
     <li className='kanban-card'>
       <div className='card-title'>{title}</div>
       <div className='card-status'>{status}</div>
+    </li>
+  )
+}
+// 看板添加状态
+const KanbanNewCard = () => {
+  return (
+    <li className='kanban-card'>
+      <h3>添加新卡片</h3>
+      <div className='card-title'>
+        <input type="text" />
+      </div>
     </li>
   )
 }
@@ -61,8 +84,9 @@ function App() {
       </header>
       <main className='kanban-board'>
           <section className='kanban-column column-todo'>
-            <h2>待处理</h2>
+            <h2>待处理 <button>添加新卡片</button></h2>
             <ul>
+            <KanbanNewCard />
             {/* {
               new Array(5).fill(0).map((item, index) => {
                 return <li className='kanban-card'>
